700d621a291734a3d12983217f35202ee62daeda
Jonathan Finger <jaefinger@gmail.com>
2020-03-09 09:08:55 -0500

    test push

--- pages/index.py
+++ pages/index.py
@@ -16,17 +16,13 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
-            ## Your Value Proposition
+            ## Coming soon:
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
-
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
-
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
+            ✅ Check back for my build sprint #2 project!
 
             """
         ),
-        dcc.Link(dbc.Button('Your Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('See predictions', color='primary'), href='/predictions')
     ],
     md=4,
 )
